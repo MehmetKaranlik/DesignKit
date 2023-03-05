a55e8e1746c2f33613dce7d8bd5b54020385a534
Mehmet Karanlık <89414084+MehmetKaranlik@users.noreply.github.com>
2023-03-05 15:08:52 +0300

    Add public to page padding

--- Sources/DesignKit/Padding/PagePadding.swift
+++ Sources/DesignKit/Padding/PagePadding.swift
@@ -10,7 +10,7 @@ import SwiftUI
 
 @available(iOS 13.0, *)
 @available(macOS 10.15, *)
-public protocol EdgeInsetsTypes {
+ protocol EdgeInsetsTypes {
 
    var size: AppSizes { get set }
    var xxSmall: EdgeInsets { get }
@@ -29,19 +29,19 @@ public protocol EdgeInsetsTypes {
 public struct PagePadding {
    private let size: AppSizes? = DesignKit.shared.appSizes
 
-   lazy var all = All(size: self.size!)
-   lazy var horizontal = Horizontal(size: self.size!)
-   lazy var top = Top(size: self.size!)
-   lazy var bottom = Bottom(size: self.size!)
-   lazy var left = Left(size: self.size!)
-   lazy var right = Right(size: self.size!)
+   public lazy var all = All(size: self.size!)
+   public lazy var horizontal = Horizontal(size: self.size!)
+   public lazy var top = Top(size: self.size!)
+   public  lazy var bottom = Bottom(size: self.size!)
+   public lazy var left = Left(size: self.size!)
+   public  lazy var right = Right(size: self.size!)
 
    init() {
       assert(size != nil, "DesignKit has not been initialized")
    }
 
    @available(iOS 13.0, *)
-   enum Custom  {
+   public enum Custom  {
       case all(value: CGFloat)
       case vertical(value: CGFloat)
       case horizontal(value: CGFloat)
@@ -50,7 +50,7 @@ public struct PagePadding {
       case top(value: CGFloat)
       case bottom(value: CGFloat)
 
-      func toValue() -> EdgeInsets {
+      public  func toValue() -> EdgeInsets {
          switch self {
             case .all(value: let value):
                return EdgeInsets(top: value, leading: value, bottom: value, trailing: value)
@@ -73,36 +73,36 @@ public struct PagePadding {
 
 @available(iOS 13.0, *)
 @available(macOS 10.15, *)
-struct All: EdgeInsetsTypes {
-   var xxSmall: EdgeInsets {
+public struct All: EdgeInsetsTypes {
+   public var xxSmall: EdgeInsets {
       return EdgeInsets(top: size.xxSmall, leading: size.xxSmall, bottom: size.xxSmall, trailing: size.xxSmall)
    }
 
-   var xSmall: EdgeInsets {
+   public  var xSmall: EdgeInsets {
       return EdgeInsets(top: size.xSmall, leading: size.xSmall, bottom: size.xSmall, trailing: size.xSmall)
    }
 
-   var standart: EdgeInsets {
+   public  var standart: EdgeInsets {
       return EdgeInsets(top: size.standart, leading: size.standart, bottom: size.standart, trailing: size.standart)
    }
 
-   var small: EdgeInsets {
+   public  var small: EdgeInsets {
       return EdgeInsets(top: size.small, leading: size.small, bottom: size.small, trailing: size.small)
    }
 
-   var medium: EdgeInsets {
+   public  var medium: EdgeInsets {
       return EdgeInsets(top: size.medium, leading: size.medium, bottom: size.medium, trailing: size.medium)
    }
 
-   var large: EdgeInsets {
+   public  var large: EdgeInsets {
       return EdgeInsets(top: size.large, leading: size.large, bottom: size.large, trailing: size.large)
    }
 
-   var xLarge: EdgeInsets {
+   public  var xLarge: EdgeInsets {
       return EdgeInsets(top: size.xLarge, leading: size.xLarge, bottom: size.xLarge, trailing: size.xLarge)
    }
 
-   var xxLarge: EdgeInsets {
+   public   var xxLarge: EdgeInsets {
       return EdgeInsets(top: size.xxLarge, leading: size.xxLarge, bottom: size.xxLarge, trailing: size.xxLarge)
    }
 
@@ -115,244 +115,242 @@ struct All: EdgeInsetsTypes {
 
 @available(iOS 13.0, *)
 @available(macOS 10.15, *)
- struct Horizontal: EdgeInsetsTypes {
-   mutating func setSize(_ size: AppSizes) {
-      self.size = size
-   }
+public  struct Horizontal: EdgeInsetsTypes {
+
 
    var size: AppSizes
 
-    var xxSmall: EdgeInsets {
+   public  var xxSmall: EdgeInsets {
        return EdgeInsets(top: 0, leading: size.xxSmall, bottom: 0, trailing: size.xxSmall)
     }
 
-    var xSmall: EdgeInsets {
+   public  var xSmall: EdgeInsets {
        return EdgeInsets(top: 0, leading: size.xSmall, bottom: 0, trailing: size.xSmall)
     }
 
-    var standart: EdgeInsets {
+   public  var standart: EdgeInsets {
        return EdgeInsets(top: 0, leading: size.standart, bottom: 0, trailing: size.standart)
     }
 
-    var small: EdgeInsets {
+   public   var small: EdgeInsets {
        return EdgeInsets(top: 0, leading: size.small, bottom: 0, trailing: size.small)
     }
 
-    var medium: EdgeInsets {
+   public  var medium: EdgeInsets {
        return EdgeInsets(top: 0, leading: size.medium, bottom: 0, trailing: size.medium)
     }
 
-    var large: EdgeInsets {
+   public  var large: EdgeInsets {
        return EdgeInsets(top: 0, leading: size.large, bottom: 0, trailing: size.large)
     }
 
-    var xLarge: EdgeInsets {
+   public  var xLarge: EdgeInsets {
        return EdgeInsets(top: 0, leading: size.xLarge, bottom: 0, trailing: size.xLarge)
     }
 
-    var xxLarge: EdgeInsets {
+   public  var xxLarge: EdgeInsets {
        return EdgeInsets(top: 0, leading: size.xxLarge, bottom: 0, trailing: size.xxLarge)
     }
 }
 
 @available(iOS 13.0, *)
 @available(macOS 10.15, *)
- struct Vertical: EdgeInsetsTypes {
+public  struct Vertical: EdgeInsetsTypes {
 
 
    var size: AppSizes
 
-    var xxSmall: EdgeInsets {
+   public var xxSmall: EdgeInsets {
        return EdgeInsets(top: size.xxSmall, leading: 0, bottom: size.xxSmall, trailing: 0)
     }
 
-    var xSmall: EdgeInsets {
+   public  var xSmall: EdgeInsets {
        return EdgeInsets(top: size.xSmall, leading: 0, bottom: size.xSmall, trailing: 0)
     }
 
-    var standart: EdgeInsets {
+   public  var standart: EdgeInsets {
        return EdgeInsets(top: size.standart, leading: 0, bottom: size.standart, trailing: 0)
     }
 
-    var small: EdgeInsets{
+   public var small: EdgeInsets{
        return EdgeInsets(top: size.small, leading: 0, bottom: size.small, trailing: 0)
     }
 
-    var medium: EdgeInsets{
+   public var medium: EdgeInsets{
        return EdgeInsets(top: size.medium, leading: 0, bottom: size.medium, trailing: 0)
     }
 
-    var large: EdgeInsets{
+   public var large: EdgeInsets{
        return EdgeInsets(top: size.large, leading: 0, bottom: size.large, trailing: 0)
     }
 
-    var xLarge: EdgeInsets{
+   public var xLarge: EdgeInsets{
        return EdgeInsets(top: size.xLarge, leading: 0, bottom: size.xLarge, trailing: 0)
     }
 
-    var xxLarge: EdgeInsets{
+   public var xxLarge: EdgeInsets{
        return EdgeInsets(top: size.xxLarge, leading: 0, bottom: size.xxLarge, trailing: 0)
     }
 }
 
 @available(iOS 13.0, *)
 @available(macOS 10.15, *)
- struct Left: EdgeInsetsTypes {
+public struct Left: EdgeInsetsTypes {
    mutating func setSize(_ size: AppSizes) {
       self.size = size
    }
 
    var size: AppSizes
 
-    var xxSmall: EdgeInsets {
+   public  var xxSmall: EdgeInsets {
        return EdgeInsets(top: 0, leading: size.xxSmall, bottom: 0, trailing: 0)
     }
 
-    var xSmall: EdgeInsets {
+   public var xSmall: EdgeInsets {
        return EdgeInsets(top: 0, leading: size.xSmall, bottom: 0, trailing: 0)
     }
 
-    var standart: EdgeInsets {
+   public var standart: EdgeInsets {
        return EdgeInsets(top: 0, leading: size.standart, bottom: 0, trailing: 0)
     }
 
-    var small: EdgeInsets {
+   public var small: EdgeInsets {
        return EdgeInsets(top: 0, leading: size.small, bottom: 0, trailing: 0)
     }
 
-    var medium: EdgeInsets {
+   public var medium: EdgeInsets {
        return EdgeInsets(top: 0, leading: size.medium, bottom: 0, trailing: 0)
     }
 
-    var large: EdgeInsets {
+   public var large: EdgeInsets {
        return EdgeInsets(top: 0, leading: size.large, bottom: 0, trailing: 0)
     }
 
-    var xLarge: EdgeInsets {
+   public var xLarge: EdgeInsets {
        return EdgeInsets(top: 0, leading: size.xLarge, bottom: 0, trailing: 0)
     }
 
-    var xxLarge: EdgeInsets {
+   public var xxLarge: EdgeInsets {
        return EdgeInsets(top: 0, leading: size.xxLarge, bottom: 0, trailing: 0)
     }
 }
 
 @available(iOS 13.0, *)
 @available(macOS 10.15, *)
- struct Right: EdgeInsetsTypes {
+public  struct Right: EdgeInsetsTypes {
 
 
    var size: AppSizes
 
-    var xxSmall: EdgeInsets {
+   public   var xxSmall: EdgeInsets {
        return EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: size.xxSmall)
     }
 
-    var xSmall: EdgeInsets {
+   public  var xSmall: EdgeInsets {
        return EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: size.xSmall)
     }
 
-    var standart: EdgeInsets {
+   public  var standart: EdgeInsets {
        return EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: size.standart)
     }
 
-    var small: EdgeInsets {
+   public  var small: EdgeInsets {
        return EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: size.small)
     }
 
-    var medium: EdgeInsets {
+   public  var medium: EdgeInsets {
        return EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: size.medium)
     }
 
-    var large: EdgeInsets {
+   public  var large: EdgeInsets {
        return EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: size.large)
     }
 
-    var xLarge: EdgeInsets {
+   public var xLarge: EdgeInsets {
        return EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: size.xLarge)
     }
 
-    var xxLarge: EdgeInsets {
+   public  var xxLarge: EdgeInsets {
        return EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: size.xxLarge)
     }
 }
 
 @available(iOS 13.0, *)
 @available(macOS 10.15, *)
-struct Top: EdgeInsetsTypes {
+public struct Top: EdgeInsetsTypes {
 
 
    var size: AppSizes
 
-   var xxSmall: EdgeInsets {
+   public  var xxSmall: EdgeInsets {
       return EdgeInsets(top: size.xxSmall, leading: 0, bottom: 0, trailing: 0)
    }
 
-   var xSmall: EdgeInsets {
+   public var xSmall: EdgeInsets {
       return EdgeInsets(top: size.xSmall, leading: 0, bottom: 0, trailing: 0)
    }
 
-   var standart: EdgeInsets {
+   public var standart: EdgeInsets {
       return EdgeInsets(top: size.standart, leading:0, bottom: 0, trailing: 0)
    }
 
-   var small: EdgeInsets {
+   public  var small: EdgeInsets {
       return EdgeInsets(top: size.small, leading: 0, bottom: 0, trailing: 0)
    }
 
-   var medium: EdgeInsets {
+   public var medium: EdgeInsets {
       return EdgeInsets(top: size.medium, leading:0 , bottom: 0, trailing: 0)
    }
 
-   var large: EdgeInsets {
+   public var large: EdgeInsets {
       return EdgeInsets(top: size.large, leading:0 , bottom: 0, trailing: 0)
    }
 
-   var xLarge: EdgeInsets {
+   public  var xLarge: EdgeInsets {
       return EdgeInsets(top: size.xLarge, leading:0 , bottom: 0, trailing: 0)
    }
 
-   var xxLarge: EdgeInsets {
+   public var xxLarge: EdgeInsets {
       return EdgeInsets(top: size.xxLarge, leading: 0, bottom: 0, trailing: 0)
    }
 }
 
 @available(iOS 13.0, *)
 @available(macOS 10.15, *)
-struct Bottom: EdgeInsetsTypes {
+public struct Bottom: EdgeInsetsTypes {
 
 
    var size: AppSizes
 
-   var xxSmall: EdgeInsets {
+   public  var xxSmall: EdgeInsets {
       return EdgeInsets(top: 0, leading: 0, bottom: size.xxSmall, trailing: 0)
    }
 
-   var xSmall: EdgeInsets {
+   public  var xSmall: EdgeInsets {
       return EdgeInsets(top: 0, leading: 0, bottom: size.xSmall, trailing: 0)
    }
 
-   var standart: EdgeInsets {
+   public var standart: EdgeInsets {
       return EdgeInsets(top: 0, leading: 0, bottom: size.standart, trailing: 0)
    }
 
-   var small: EdgeInsets {
+   public var small: EdgeInsets {
       return EdgeInsets(top: 0, leading: 0, bottom: size.small, trailing: 0)
    }
 
-   var medium: EdgeInsets {
+   public  var medium: EdgeInsets {
       return EdgeInsets(top: 0, leading: 0, bottom: size.medium, trailing: 0)
    }
 
-   var large: EdgeInsets {
+   public var large: EdgeInsets {
       return EdgeInsets(top: 0, leading: 0, bottom: size.large, trailing: 0)
    }
 
-   var xLarge: EdgeInsets {
+   public  var xLarge: EdgeInsets {
       return EdgeInsets(top: 0, leading: 0, bottom: size.xLarge, trailing: 0)
    }
 
-   var xxLarge: EdgeInsets {
+   public  var xxLarge: EdgeInsets {
       return EdgeInsets(top: 0, leading: 0, bottom: size.xxLarge, trailing: 0)
    }
 }
